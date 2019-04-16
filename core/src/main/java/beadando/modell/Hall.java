de4b7655882ad8c2623f357ddeb68c9303d56225
thomas555552 <48090490+thomas555552@users.noreply.github.com>
2019-04-16 20:07:45 +0200

    Base of mvc +-modells

--- core/src/main/java/beadando/modell/Hall.java
+++ core/src/main/java/beadando/modell/Hall.java
@@ -1,15 +1,26 @@
 package beadando.modell;
 
+import java.util.SortedMap;
+
 public class Hall {
 
     private String name;
     private int seatperrows;
     private int rowsnumber;
+    private SortedMap<Integer, User> seats;
 
     public Hall(String name, int seatperrows, int rowsnumber) {
         this.name = name;
         this.seatperrows = seatperrows;
         this.rowsnumber = rowsnumber;
+    }
+
+    public void setSeats(SortedMap<Integer, User> seats) {
+        this.seats = seats;
+    }
+
+    public SortedMap<Integer, User> getSeats() {
+        return seats;
     }
 
     public String getName() {
--- core/src/main/java/beadando/modell/Projection.java
+++ core/src/main/java/beadando/modell/Projection.java
@@ -6,6 +6,42 @@ public class Projection {
     private Film film;
     private Hall hall;
 
+    public Projection(int ID, String date, Film film, Hall hall) {
+        this.ID = ID;
+        this.date = date;
+        this.film = film;
+        this.hall = hall;
+    }
 
+    public int getID() {
+        return ID;
+    }
 
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public Hall getHall() {
+        return hall;
+    }
+
+    public void setHall(Hall hall) {
+        this.hall = hall;
+    }
 }
