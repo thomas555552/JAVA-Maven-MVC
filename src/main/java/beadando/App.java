623eeb09b118f58277227f0e782bf453a7dc80cb
thomas555552 <48090490+thomas555552@users.noreply.github.com>
2019-04-18 19:58:02 +0200

    DONE Controller,Services,Core NEED - console APP

--- src/main/java/beadando/App.java
+++ src/main/java/beadando/App.java
@@ -1,10 +1,26 @@
 package beadando;
 
 
-public class App 
-{
+import beadando.controller.FilmController;
+import beadando.controller.HallController;
+import beadando.controller.ProjectionController;
+import beadando.controller.UserController;
+import beadando.service.FilmService;
+import beadando.service.HallService;
+import beadando.service.ProjectionService;
+import beadando.service.UserService;
+
+public class App {
+
     public static void main( String[] args )
     {
+
+        FilmController filmController = new FilmController(new FilmService());
+        UserController userController = new UserController(new UserService());
+        HallController hallController = new HallController(new HallService());
+        ProjectionController projectionController = new ProjectionController(new ProjectionService());
+
+        
 
 
     }
