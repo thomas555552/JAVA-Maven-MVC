2ff7c3625db47a511f1e284e8c050b2c6cb10e43
thomas555552 <48090490+thomas555552@users.noreply.github.com>
2019-04-18 22:32:57 +0200

    view module for console app

--- view/src/main/java/beadando/view/View.java
+++ view/src/main/java/beadando/view/View.java
@@ -1,4 +1,33 @@
 package beadando.view;
 
+import beadando.controller.FilmController;
+import beadando.modell.Film;
+import beadando.service.FilmService;
+
 public class View {
+
+
+    public static void main( String[] args )
+    {
+
+        FilmController filmController = new FilmController(new FilmService());
+
+        System.out.println("ASD");
+
+        filmController.addFilm(new Film("asd","qwe",2014, 150));
+
+        System.out.println("Director: " +filmController.getFilmByTitle("asd").getDirector());
+/*
+        FilmController filmController = new FilmController(new FilmService());
+        UserController userController = new UserController(new UserService());
+        HallController hallController = new HallController(new HallService());
+        ProjectionController projectionController = new ProjectionController(new ProjectionService());
+
+        filmController.addFilm(new Film("asd","qwe",2014, 150));
+
+        System.out.println("Director: " +filmController.getFilmByTitle("asd").getDirector());
+*/
+    }
+
+
 }
