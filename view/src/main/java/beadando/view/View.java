c091631f2ec63a92a33fd42252bc9d863513142f
thomas555552 <48090490+thomas555552@users.noreply.github.com>
2019-04-18 23:04:36 +0200

    View OK - just for test
    Need to RESOLVE HALL and OTHERS

--- view/src/main/java/beadando/view/View.java
+++ view/src/main/java/beadando/view/View.java
@@ -1,8 +1,17 @@
 package beadando.view;
 
 import beadando.controller.FilmController;
+import beadando.controller.HallController;
+import beadando.controller.ProjectionController;
+import beadando.controller.UserController;
 import beadando.modell.Film;
+import beadando.modell.Hall;
+import beadando.modell.Projection;
+import beadando.modell.User;
 import beadando.service.FilmService;
+import beadando.service.HallService;
+import beadando.service.ProjectionService;
+import beadando.service.UserService;
 
 public class View {
 
@@ -11,23 +20,32 @@ public class View {
     {
 
         FilmController filmController = new FilmController(new FilmService());
-
-        System.out.println("ASD");
-
-        filmController.addFilm(new Film("asd","qwe",2014, 150));
-
-        System.out.println("Director: " +filmController.getFilmByTitle("asd").getDirector());
-/*
-        FilmController filmController = new FilmController(new FilmService());
         UserController userController = new UserController(new UserService());
         HallController hallController = new HallController(new HallService());
         ProjectionController projectionController = new ProjectionController(new ProjectionService());
 
-        filmController.addFilm(new Film("asd","qwe",2014, 150));
 
-        System.out.println("Director: " +filmController.getFilmByTitle("asd").getDirector());
-*/
+        addDataBeforeStart(filmController,userController,hallController,projectionController);
+
+
+        hallController.setUserToMoreSeat(hallController.getHallbyName("Nagy terem"), "101,102,103", userController.findByUsername("User1"));
+
+        System.out.println(hallController.getFreeSeats(hallController.getHallbyName("Nagy terem")));
+
     }
 
+    public static void addDataBeforeStart(FilmController Fcontroller, UserController Ucontroller, HallController Hcontroller, ProjectionController Pcontroller){
+        Fcontroller.addFilm(new Film("TestTitle1", "TestDirector1", 2019, 90));
+        Fcontroller.addFilm(new Film("TestTitle2", "TestDirector2", 2018, 150));
+        Fcontroller.addFilm(new Film("TestTitle3", "TestDirector3", 2017, 100));
+
+        Ucontroller.add(new User("User1", "Valami utca 2", 20));
+        Ucontroller.add(new User("User2", "Kis utca 4", 50));
+
+        Hcontroller.addHall(new Hall("Nagy terem", 5, 3));
+
+        Pcontroller.addProjection(new Projection(1,"2019.05.20, 18:00", Fcontroller.getFilmByTitle("TestTitle2"), Hcontroller.getHallbyName("Nagy terem")));
+
+    }
 
 }
